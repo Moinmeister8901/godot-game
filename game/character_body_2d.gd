2b63a60bebd640d439b0fb944045d0902e67247f
ihatekaisfamily <ballballer057@gmail.com>
2025-05-28 20:30:33 +0200

    new proto-type available now!

--- game/character_body_2d.gd
+++ game/character_body_2d.gd
@@ -1,21 +1,20 @@
 extends CharacterBody2D
 
-var movement = Vector2(0, 0)
 var speed = 150
+var gravity = 1000
+var jump = 500
 
 func _physics_process(delta):
-	if  Input.is_action_just_pressed("ui_right"):
-		movement = Vector2(speed, 0)
+	var velocity = self.velocity
+	velocity.y += gravity * delta
+	if  Input.is_action_just_pressed("ui_up") and is_on_floor():
+		velocity.y = -jump
+	elif  Input.is_action_just_pressed("ui_right"):
+		velocity.x = speed
 	elif Input.is_action_just_pressed("ui_left"):
-		movement= Vector2(-speed, 0)
-	elif Input.is_action_just_pressed("ui_up"):
-		movement = Vector2(0, -speed)
-	elif Input.is_action_just_pressed("ui_down"):
-		movement = Vector2(0, speed)
-	elif Input.is_action_just_released("ui_right") or Input.is_action_just_released("ui_left") or Input.is_action_just_released("ui_up") or Input.is_action_just_released("ui_down"):
-		movement = Vector2(0, 0)
-
+		velocity.x = -speed
+	elif Input.is_action_just_released("ui_right") or Input.is_action_just_released("ui_left"):
+		velocity.x = 0
 		
-		
-	move_and_collide(movement * delta)
+	self.velocity = velocity
 	move_and_slide()
